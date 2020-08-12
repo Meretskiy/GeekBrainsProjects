f8bcefe8af3bdbc331a9c8e1875917f2dcc2a371
Meretskiy Aleksandr <a-meretskiy@mail.ru>
2020-08-13 00:06:17 +0300

    update hw21(java3_5)

--- homework/src/main/java/ru/meretskiy/homework21_concurrent/Car.java
+++ homework/src/main/java/ru/meretskiy/homework21_concurrent/Car.java
@@ -1,8 +1,13 @@
 package ru.meretskiy.homework21_concurrent;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
 
     private static int CARS_COUNT;
+    private static CyclicBarrier cyclicBarrier;
+    private static AtomicInteger atomicInteger = new AtomicInteger();
 
     static {
         CARS_COUNT = 0;
@@ -20,6 +25,18 @@ public class Car implements Runnable {
         return speed;
     }
 
+    public static CyclicBarrier getCyclicBarrier() {
+        return cyclicBarrier;
+    }
+
+    public static void setCyclicBarrier(CyclicBarrier cyclicBarrier) {
+        Car.cyclicBarrier = cyclicBarrier;
+    }
+
+    public static AtomicInteger getAtomicInteger() {
+        return atomicInteger;
+    }
+
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
@@ -33,11 +50,16 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        atomicInteger.incrementAndGet();
+        if (atomicInteger.get() == 1) {
+            System.out.println(this.name + " WIN!!!");
         }
     }
 }
--- homework/src/main/java/ru/meretskiy/homework21_concurrent/MainClass.java
+++ homework/src/main/java/ru/meretskiy/homework21_concurrent/MainClass.java
@@ -1,20 +1,39 @@
 package ru.meretskiy.homework21_concurrent;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
 
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+        Car.setCyclicBarrier(new CyclicBarrier(CARS_COUNT + 1));
+        Tunnel.setSemaphore(new Semaphore(CARS_COUNT / 2));
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        try {
+            Car.getCyclicBarrier().await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        while (Car.getAtomicInteger().get() != CARS_COUNT);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
